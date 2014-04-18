8ee08d0eeced9eaaedcc9c802a940c4b4ce271ce
daniel dropik <ddropik@acs.wisc.edu>
2014-04-18 10:17:06 -0500

    Fix gitignore to prevent it from being ignored

--- home.php
+++
@@ -1,4 +0,0 @@
-<?php get_header(); ?>
-<div><p>this is home.php, this is used to render the 'posts page' when you select front page displays-->static page.</p></div>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
