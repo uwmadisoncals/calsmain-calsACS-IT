78de1498bf0330ac44aa9e2df6e5afcdbde2e0f1
daniel dropik <ddropik@acs.wisc.edu>
2014-04-18 10:34:06 -0500

    again trying to fix .gitignore

--- front-page.php
+++
@@ -1,6 +0,0 @@
-<?php get_header(); ?>
-
-<div class="row cb-theme"><div class="box bottomShade span-50"><p>this is front-page.php, this is used to render the 'front page' when you select front page displays -static page.</p></div></div>
-
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
