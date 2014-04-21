824ea85df1ce6fb8537d418965fa63b339c1a900
daniel dropik <ddropik@acs.wisc.edu>
2014-04-21 10:07:44 -0500

    Add CSS rules to style the margins and layout of the divs on the main page. Add HTML content as place holder content.

--- front-page.php
+++ front-page.php
@@ -1,6 +1,28 @@
 <?php get_header(); ?>
 
-<div class="row cb-theme"><div class="box bottomShade span-50"><p>this is front-page.php, this is used to render the 'front page' when you select front page displays -static page.</p></div></div>
+<div id="main">
+	<div id="primary">
+		<div id="content">
+			<div class="row">
+				<div id="box-a" class="box-400 bottomShade span-66">
+					<div id="FS-title"><h2>Featured service</h2></div>
+					<div id="FS-image"><img src="http://acs.calsmain.localhost/files/2014/04/ga.jpg" alt="noen"></div>
+					<div id="FS-content"><p>featured service here</p></div>
+				</div>
+			</div>
+			<div class="row">
+				<div id="box-b" class="box bottomShade span-33">
+					<div id="news-title"><h2>News</h2></div>
+					<div class="news-content"><p>News Content here</p></div>
+				</div>
+			</div>
+			<div class="row">
+				<div id="box-c" class="box bottomShade span-33"><p>this is front-page.php.</p>	
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
