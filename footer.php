c526bf757823ce57083c323b684273aafd9558ba
daniel dropik <ddropik@acs.wisc.edu>
2014-07-18 13:48:57 -0500

    adding footer.php, adding markup
    create new sass file for the footer, include some styles with it
    
    including the @import in master.scss sincee I made a new sass file for footer

--- footer.php
+++ footer.php
@@ -12,10 +12,33 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'calsboilerplate_underscores' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'calsboilerplate_underscores' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'calsboilerplate_underscores' ), 'CALSboilerplate_underscores', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			<!--<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'calsboilerplate_underscores' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'calsboilerplate_underscores' ), 'CALS Web Hosting' ); ?></a> -->
 		</div><!-- .site-info -->
+		<div class="darkband"></div>
+		<div class="contactInfo">
+		<img src="" alt="" class="logo">
+		<div class="uwaddress">
+			<h3>CALS IT ACS</h3>
+			<a href="">University of Wisconsin Madison</a>
+			<address>
+				<span class="street">728 State Street</span>
+				<span>• Madison, WI</span>
+				<span>• 53706</span>
+			</address>
+			<div class="phone">(608)262-3193</div>
+			<div class="links">
+				<a href="">Contact Us</a>
+				<a href="">• Accessibility</a>
+				<a href="">• Code-of-conduct</a>
+			</div>
+		</div>
+			
+		</div>
+		<div class="support">
+			
+		</div>
+
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
