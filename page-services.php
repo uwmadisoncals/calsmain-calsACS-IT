d94e0377bdbd65052381bb393820f162213128ab
daniel dropik <ddropik@acs.wisc.edu>
2014-09-29 10:29:04 -0500

    page-services.php: removed the codrops menu markup. __vendor__.scss: corrected the @include file path split-layout.scss: removed css for the codrops menu. changed positioning, width and height of headerContainer to make it visible. Changed the top positioning of the left and right side.

--- page-services.php
+++ page-services.php
@@ -22,20 +22,7 @@ get_header(); ?>
 			<div id="splitlayout" class="splitlayout">
 				<div class="intro">
 					<div class="side side-left">
-						<header class="codropsheader clearfix">
-							<span>Blueprint <span class="bp-icon bp-icon-about" data-content="The Blueprints are a collection of basic and minimal website concepts, components, plugins and layouts with minimal style for easy adaption and usage, or simply for inspiration."></span></span>
-							<h1>Split Layout</h1>
-							<nav>
-								<a href="http://tympanus.net/Blueprints/OnScrollEffectLayout/" class="bp-icon bp-icon-prev" data-info="previous Blueprint"><span>Previous Blueprint</span></a>
-								<!--a href="" class="bp-icon bp-icon-next" data-info="next Blueprint"><span>Next Blueprint</span></a-->
-								<a href="http://tympanus.net/codrops/?p=16693" class="bp-icon bp-icon-drop" data-info="back to the Codrops article"><span>back to the Codrops article</span></a>
-								<a href="http://tympanus.net/codrops/category/blueprints/" class="bp-icon bp-icon-archive" data-info="Blueprints archive"><span>Go to the archive</span></a>
-							</nav>
-							<div class="demos">
-								<a class="current" href="index.html">Effect 1</a>
-								<a href="index2.html">Effect 2</a>
-							</div>
-						</header>
+					
 						<div class="intro-content">
 							<div class="profile"><img src="img/hosting.svg" alt="profile1"></div>
 							<h1><span>CALS IT</span><span>Information Technology</span></h1>
